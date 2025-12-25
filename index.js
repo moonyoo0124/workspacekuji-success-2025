eec6dfc80ee5a4c08c3cd0456d3f692cb752db31
MoonYoo <moony240224@gmail.com>
2025-12-25 07:06:51 +0000

    fix error

--- index.js
+++ index.js
@@ -141,6 +141,10 @@ app.post("/draw", async (req, res) => {
   return res.json({ result: result });
 });
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
